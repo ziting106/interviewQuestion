cd0340d1d4ef47729f3908bba05d78a1467adb02
Ziting Liu <zitingliu@systex.com>
2024-12-05 13:41:25 +0800

    click呈現選單、blur選單消失

--- components/multiple-select/multiple-select.js
+++ components/multiple-select/multiple-select.js
@@ -1,7 +1,7 @@
 export class MultipleSelect extends HTMLElement{
   // 計畫：
   // 0.變數設定
-  // 1.點擊input觸發select出現
+  // 1.點擊input觸發select出現 => 試著直接用select就好
   // 2.在option新增勾選符號
   // 3.點擊選項觸發"勾選"、"Ctrl"、"更新input value"
   // 4.onBlur後隱藏select
@@ -11,22 +11,35 @@ export class MultipleSelect extends HTMLElement{
     const selectList = this.getAttribute("selectList") || [{ value:'', name:'--查無資料--'}];
     const placeholder = this.getAttribute("placeholder") || '請選擇';
     const label = this.getAttribute("label") || '欄位標題';
-
     this.attachShadow({ mode: "open" });
 
     this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="/components/multiple-select/multiple-select.css">
       <div class="d-flex">
         <label for="mulSelect" class="noWrap">${label}：</label>
-        <div>
-          <input class="w-100 mulInput" type="select" placeholder="${placeholder}" readonly>
-          <select class="w-100 mulSelect" name="pets" id="mulSelect" multiple>
+        <div class="d-flex flex-direction-column">
+          <input class="ww-200 mulInput" type="select" placeholder="${placeholder}" readonly id="selectInput">
+          <select class="ww-200 d-none" name="pets" id="mulSelect" multiple>
             ${selectList.map( item => `<option value="${item.value}">${item.name}</option>`).join("")}
           </select>
         </div>
       </div>
     `;
+    const selectInput = this.shadowRoot.getElementById("selectInput");
+    const selectItem = this.shadowRoot.getElementById("mulSelect");
+    selectInput.addEventListener("click",  isSelectOpen.bind(this,true));
+    selectItem.addEventListener("blur",  isSelectOpen.bind(this,false));
+    
+    function isSelectOpen(status) {
+      const mulSelect = this.shadowRoot.getElementById("mulSelect");
+      status?mulSelect.classList.remove("d-none"):mulSelect.classList.add("d-none");
+      if(status){
+        selectItem.focus();
+      }
+    }
   }
+  
+  
   // 接受傳來的selectList data
   set data(data){
     const selectOption = this.shadowRoot.querySelector("select")
